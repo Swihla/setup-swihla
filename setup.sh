69d09181baa43b5f0e78576529e43dab62742c25
lhoussaine bouaik <bouaik.lhou@gmail.com>
2021-05-30 20:45:12 +0100

    Add comments to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
+
+# Clone Repos
 git clone git@github.com:Swihla/api-swihla.git
 
 git clone git@github.com:Swihla/front-swihla.git
 
+# Build Containers
 docker-compose build
 
+# Install Rails Dependencies
 docker-compose run --rm --no-deps web bundle install
 
 docker-compose run --rm --no-deps web yarn install
 
+# Install React Dependencies
 docker-compose run --rm --no-deps client yarn install
 
+# Create Rails DB and Migrate
 docker-compose run --rm --no-deps web rails db:create db:migrate
